302c23da99481631f17727334894e1d4f138199b
manu <manu@manhattan>
2016-04-29 15:01:07 +0200

    5 cross valivation ok

--- src/Driver_Operations/Driver.java
+++ src/Driver_Operations/Driver.java
@@ -7,6 +7,7 @@ package Driver_Operations;
 
 
 import Hdfs_Operations.HdfsReaderExt;
+import Hdfs_Operations.HdfsRemove;
 import Hdfs_Operations.HdfsWriter;
 import Hive_JDBC_Operations.DataBase;
 import Parse.ParseFileFromLocal;
@@ -17,6 +18,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 import org.apache.hadoop.fs.Path;
@@ -48,6 +50,8 @@ public class Driver extends Configured implements Tool
     
     public static void main(String[] args) throws Exception
     {
+        String[] args2 = new String[2];
+        
         if(args.length < 4)
         {
             System.err.println("Number of arguments incorrect");
@@ -61,29 +65,37 @@ public class Driver extends Configured implements Tool
         ParseFileFromLocal.parse(args[0], args[1], Integer.parseInt(args[2]));
         System.out.println("$$$$$$$$$$$$$$$$$$$$$-> Data warehouse OK");
         //--
-        /*
-        //Generamos el fichero a través del cual se lanzarán los Map
-        Driver.generateBigFile();
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$-> Dataset write in HDFS OK");
         
+        //Generamos 5 cross validation
+        for (int i = 1; i <= 5; i++)
+        {
+            //Borramos el directorio de salida de trabajos MapReduce
+            args2[0] = "/output";
+            args2[1] = "";
+            ToolRunner.run(new HdfsRemove(), args2);
+            
+            //Generamos el fichero a través del cual se lanzarán los Map
+            Driver.generateBigFile(i);
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$-> Dataset write in HDFS OK");
+
+
+            //Lanzamos la tarea
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$-> Starting job " + i);
+            ToolRunner.run(new Driver(), args);
+            
+            //args2[0] = "/output/part-r-00000";
+            args2[0] = "/output/part-r-00000";
+            args2[1] = args[3] + i;
+
+            //Escribimos el resultado en el almacenamiento local
+            ToolRunner.run(new HdfsReaderExt(), args2);
+
+            //Escritura del fichero de entrada de los MAP en el almacenamiento local
+            args2[0] = "/input/dataset";
+            args2[1] = "/home/manu/datasetHDFS" + i;
+            ToolRunner.run(new HdfsReaderExt(), args2);
+        }
         
-        //Lanzamos la tarea
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$-> Starting");
-        ToolRunner.run(new Driver(), args);
-        
-        String[] args2 = new String[2];
-        //args2[0] = "/output/part-r-00000";
-        args2[0] = "/output/part-r-00000";
-        args2[1] = args[3];
-        
-        //Escribimos el resultado en el almacenamiento local
-        ToolRunner.run(new HdfsReaderExt(), args2);
-        /*
-        //Escritura del fichero de entrada de los MAP en el almacenamiento local
-        args2[0] = "/input/dataset";
-        args2[1] = "/home/manu/datasetHDFS";
-        ToolRunner.run(new HdfsReaderExt(), args2);
-        */
         System.exit(0);
         
     }
@@ -117,26 +129,41 @@ public class Driver extends Configured implements Tool
     
     public static float calculateIR()
     {
-        long numNeg = DataBase.getNumRows(Driver.nameTableNeg);
-        long numPos = DataBase.getNumRows(Driver.nameTablePos);
+        long numNeg = DataBase.getNumRows(Driver.nameTableTrainingNeg);
+        long numPos = DataBase.getNumRows(Driver.nameTableTrainingPos);
         
         return (float)numNeg/numPos;
     }
     
     
-    public static void generateBigFile() throws Exception
+    public static void generateBigFile(int testFold) throws Exception
     {
         String positiveExamples, line;
-        float iR;
         long positiveSize, negativeSize;
         long value, sizeSubsection;
-        int[] numBits;
+        int[] numBits, trainingFolds = new int[4];
+        float iR;
+        
+        //Escogemos los folds para training
+        for (int i=1, j=0; i <= 5; i++, j++)
+        {
+            if(i != testFold)
+                trainingFolds[j] = i;
+
+            else j--;
+        }
+        
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$-> Training folds " + Arrays.toString(trainingFolds));
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$-> Test fold: " + testFold);
+        
+        //Construimos las tablas
+        DataBase.createTrainingTable(Driver.nameBigTablePos, Driver.nameTableTrainingPos, trainingFolds);
+        DataBase.createTrainingTable(Driver.nameBigTableNeg, Driver.nameTableTrainingNeg, trainingFolds);
         
         numBits = ParseFileFromLocal.getNumBits();
-        iR = Driver.calculateIR();
-        positiveSize = DataBase.getNumRows(Driver.nameTablePos);
-        negativeSize = DataBase.getNumRows(Driver.nameTableNeg);
-        positiveExamples = DataBase.getDataBinaryFormat(Driver.nameTablePos, numBits, 1, positiveSize);
+        positiveSize = DataBase.getNumRows(Driver.nameTableTrainingPos);
+        negativeSize = DataBase.getNumRows(Driver.nameTableTrainingNeg);
+        positiveExamples = DataBase.getDataBinaryFormat(Driver.nameTableTrainingPos, numBits, 1, positiveSize);
         
         //Obtenemos el buffer de escritura en HDFS
         ToolRunner.run(new HdfsWriter(), new String[] {"/input/dataset"});
@@ -144,11 +171,12 @@ public class Driver extends Configured implements Tool
         
         //El IR del dataset que recibe el genético va a ser 1
         int irAGL = 1;
+        iR = Driver.calculateIR();
         
         for (int i = 0; i < (int)iR; i++)
         {
             value = (i*positiveSize)+1;
-            line = irAGL + "\t" + positiveExamples + DataBase.getDataBinaryFormat(Driver.nameTableNeg, numBits, value, value+positiveSize-1) + "\n";
+            line = irAGL + "\t" + positiveExamples + DataBase.getDataBinaryFormat(Driver.nameTableTrainingNeg, numBits, value, value+positiveSize-1) + "\n";
             br.write(line);//Escribimos en HDFS
         }
         
@@ -159,9 +187,9 @@ public class Driver extends Configured implements Tool
             value = (((int)iR)*positiveSize)+1;
             sizeSubsection = positiveSize - (negativeSize - (((int)iR) * positiveSize));
             //Obtenemos el spilt final
-            line = irAGL + "\t" + positiveExamples + DataBase.getDataBinaryFormat(Driver.nameTableNeg, numBits, value, negativeSize);
+            line = irAGL + "\t" + positiveExamples + DataBase.getDataBinaryFormat(Driver.nameTableTrainingNeg, numBits, value, negativeSize);
             //le añadimos el trozo faltante con ejemplos del primer split
-            line += DataBase.getDataBinaryFormat(Driver.nameTableNeg, numBits, 1, sizeSubsection) + "\n";
+            line += DataBase.getDataBinaryFormat(Driver.nameTableTrainingNeg, numBits, 1, sizeSubsection) + "\n";
             br.write(line);//Escribimos en HDFS
         }
         
